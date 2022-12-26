6df3a205fef6da117c859c218348235099b3458a
matthiasmulzet <mulzetmatthias@gmail.com>
2022-12-26 15:08:58 +0100

    added angularfire to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,12 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { MatCardModule } from '@angular/material/card';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 
 
 @NgModule({
@@ -27,6 +33,7 @@ import { MatCardModule } from '@angular/material/card';
     GameInfoComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(environment.firebase),
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
@@ -35,7 +42,11 @@ import { MatCardModule } from '@angular/material/card';
     MatInputModule,
     FormsModule,
     MatCardModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'ringoffire-bcd6f',
+    appId: '1:893544114160:web:fa7779e5eed7653d301f35',
+    storageBucket: 'ringoffire-bcd6f.appspot.com',
+    apiKey: 'AIzaSyC8utX3OGL8vYQU3xXs9QE9wUa2rQE3-Cs',
+    authDomain: 'ringoffire-bcd6f.firebaseapp.com',
+    messagingSenderId: '893544114160',
+  }
+};
+
+
+
