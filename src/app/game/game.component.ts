cf9399fdbb8584ba4d97c5f4acfc753421139a2f
matthiasmulzet <mulzetmatthias@gmail.com>
2022-12-26 19:12:48 +0100

    connected database to project

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/models/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
 
 @Component({
   selector: 'app-game',
@@ -14,12 +16,23 @@ export class GameComponent implements OnInit {
   game: Game;
 
 
-  constructor(public dialog: MatDialog) {
+
+  constructor(private firestore: AngularFirestore, public dialog: MatDialog) {
     this.game = new Game();
   }
 
+
+
+
   ngOnInit(): void {
     this.newGame();
+    this
+      .firestore
+      .collection('games')
+      .valueChanges()
+      .subscribe((game) => {
+        console.log('game update', game);
+      })
   }
 
   newGame() {
